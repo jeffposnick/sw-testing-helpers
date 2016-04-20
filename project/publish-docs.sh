1a17548ef124f7b9a06b96487a66259ec8a82cd7
Matthew  Gaunt <matt@gauntface.co.uk>
2016-04-20 15:04:12 +0100

    Fix the order of the docs (#27)

--- project/publish-docs.sh
+++ project/publish-docs.sh
@@ -39,6 +39,11 @@ if [ -z "$1" ]; then
 fi
 
 if [ "$TRAVIS" ]; then
+  if [ "$TRAVIS_OS_NAME" = "osx" ]; then
+    echo "In OS X on travis so skipping doc deployment."
+    exit 0;
+  fi
+
   if [[ "$TRAVIS_BRANCH" != "master" || "$TRAVIS_PULL_REQUEST" != "false" ]]; then
     echo "In a travis build but not master branch so skipping doc deployment."
     exit 0;
@@ -85,6 +90,39 @@ cd ..
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cp -r $SCRIPTPATH/../docs-template/. ./gh-pages/
+
+echo ""
+echo ""
+echo "Configure Doc Directories in _data/gendoclist.yml"
+echo ""
+
+cd ./gh-pages
+mkdir -p _data
+DOCS_INFO_OUTPUT="./_data/gendoclist.yml"
+echo "# Auto-generated from the sw-testing-helper module" >> $DOCS_INFO_OUTPUT
+echo "releases:" >> $DOCS_INFO_OUTPUT
+RELEASE_DIRECTORIES=$(find ./docs/releases/ -maxdepth 1 -mindepth 1 -type d -printf '%f\n' | sort --version-sort --reverse);
+for releaseDir in $RELEASE_DIRECTORIES; do
+  if [ -f ./docs/releases/$releaseDir/index.html ]; then
+    echo "    - $releaseDir" >> $DOCS_INFO_OUTPUT
+  else
+    echo "Skipping releases/$releasesDir due to no index.html file"
+  fi
+done
+echo "docs:" >> $DOCS_INFO_OUTPUT
+DOC_DIRECTORIES=$(find ./docs/ -maxdepth 1 -mindepth 1 -type d -printf '%f\n');
+for docDir in $DOC_DIRECTORIES; do
+  if [ "$docDir" = 'releases' ]; then
+    continue
+  fi
+
+  if [ -f ./docs/$docDir/index.html ]; then
+    echo "  - $docDir" >> $DOCS_INFO_OUTPUT
+  else
+    echo "Skipping $docDir due to no index.html file"
+  fi
+done
+cd ..
 
 echo ""
 echo ""
